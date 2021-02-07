ed19065a4defc6f23bb0640d4ed070dfec83687b
José Roberto <tekinforroberto@gmail.com>
2021-02-06 23:23:11 -0300

    Testando typescript com Gulp

---
+++ rodando_gulp/public/teste.js
@@ -0,0 +1,5 @@
+function bemvindo(nome) {
+    return "Olá " + nome;
+}
+var n = "José Roberto";
+console.log(bemvindo(n));
---
+++ rodando_gulp/src/teste.ts
@@ -0,0 +1,8 @@
+function bemvindo(nome: string) {
+    return "Olá "+nome;
+}
+
+let n = "José Roberto";
+
+console.log(bemvindo(n));
+
